2200dcddcf18313cff56b6b0126e6ba90e16fd6f
Stefano Di Martino <stefano.d@posteo.de>
2016-07-19 20:39:59 +0200

    in place variant added

--- src/rand_choice.rs
+++ src/rand_choice.rs
@@ -1,16 +1,19 @@
+/// Implementation of Stochastic universal sampling
+/// https://en.wikipedia.org/wiki/Stochastic_universal_sampling
+/// Runtime: O(n)
+/// Memory Usage: O(n) or O(1) (in place variant) 
+
 extern crate rand;
 
 use self::rand::{thread_rng, Rng};
 
-/**
-* Chooses n samples by their weights. The greater their weights the more likely they get chosen.
-*
-* @invariant sum of weights must not overflow.
-* @param samples The to be selected samples
-* @param weights Weights that get chosen by their weight/probability. One weight can be greater 1.
-* @param n Number of randomly chosen indices by weight.
-* @return randomly selected samples by their weight
-*/
+/// Chooses n samples by their weights. The greater their weights the more likely they get chosen.
+///
+/// @invariant sum of weights must not overflow.
+/// @param samples The to be selected samples
+/// @param weights Weights that get chosen by their weight/probability. One weight can be greater 1.
+/// @param n Number of randomly chosen indices by weight.
+/// @return randomly selected samples by their weights
 pub fn random_choice<'a, T>(samples: &'a Vec<T>, weights: &Vec<f64>, n: usize) -> Vec<&'a T>{
     // TODO Check, if weight.len() > 0 and n > 0
 
@@ -37,11 +40,34 @@ pub fn random_choice<'a, T>(samples: &'a Vec<T>, weights: &Vec<f64>, n: usize) -
     choices
 }
 
+pub fn random_choice_in_place<T: Copy>(samples: &mut Vec<T>, weights: &Vec<f64>) {
+    // TODO Check, if weight.len() > 0 and n > 0
+    let sum:f64 = weights.iter().fold(0.0, |acc, &i| acc + i);
+    let n: usize = weights.len();
+    let spoke_gap: f64 = sum / n as f64;
+
+    // next_f64() ∈ [0.0, 1.0)
+    let spin = thread_rng().next_f64() * spoke_gap;
+    
+    let mut j: usize = 0;
+    let mut accumulated_weights = weights[0];
+    let mut current_spoke: f64 = spin;
+
+    for i in 0 .. n {
+        while accumulated_weights < current_spoke {
+            j += 1;
+            accumulated_weights += weights[j];
+        }
+        samples[i] = samples[j];
+        current_spoke += spoke_gap;
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn basics() {
+    fn test_random_choice() {
         let test_vec: Vec<f64> = vec![3.1, 1.2, 1.3, 2.0];
         //let sum:f64 = test_vec.iter().fold(0.0, |acc, &i| acc + i);
 
